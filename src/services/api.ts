aa9e799f424d1bb409fc02ab8244f32af297cdbb
Henry Quispe Huayta <henry.quispe.huayta@gmail.com>
2025-02-19 10:22:14 -0400

    Add login service to customer API methods for authentication

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,9 +2,20 @@ import axios from "axios";
 import { User, CreateUserInput } from "../types/user";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const USER = import.meta.env.VITE_PONTIS_LOGIN_USER;
+const PASSWORD = import.meta.env.VITE_PONTIS_LOGIN_PASSWORD;
+
+const loginService = async () => {
+  const response = await axios.post(`${API_BASE_URL}/auth/login`, {
+    customer_id: USER,
+    password: PASSWORD,
+  });
+  return response;
+};
 
 export const customerApi = {
   getCustomerById: async (customerId: string) => {
+    await loginService();
     const response = await axios.post(`${API_BASE_URL}/customers/getCustomer`, {
       customerId,
     });
@@ -16,6 +27,7 @@ export const customerApi = {
   },
 
   deleteCustomer: async (customerId: string) => {
+    await loginService();
     try {
       const response = await axios.post(`${API_BASE_URL}/customers/delete`, {
         customerId,
@@ -34,6 +46,7 @@ export const customerApi = {
   },
 
   createCustomer: async (userData: CreateUserInput) => {
+    await loginService();
     await new Promise((resolve) => setTimeout(resolve, 500));
     const newUser = { ...userData };
     // mockUsers.push(newUser);
@@ -41,6 +54,7 @@ export const customerApi = {
   },
 
   updateCustomer: async (userData: Partial<User>) => {
+    await loginService();
     await new Promise((resolve) => setTimeout(resolve, 500));
     const index = mockUsers.findIndex(
       (u) => u.customer.customerId === userData.customer?.customerId
@@ -53,6 +67,7 @@ export const customerApi = {
   },
 
   inactivateCustomer: async (customerId: string) => {
+    await loginService();
     await new Promise((resolve) => setTimeout(resolve, 500));
     const user = mockUsers.find((u) => u.customer.customerId === customerId);
     if (user) {
@@ -61,6 +76,7 @@ export const customerApi = {
   },
 
   activateCustomer: async (customerId: string) => {
+    await loginService();
     await new Promise((resolve) => setTimeout(resolve, 500));
     const user = mockUsers.find((u) => u.customer.customerId === customerId);
     if (user) {
